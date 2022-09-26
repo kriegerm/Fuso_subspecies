8fd3d68afb03f12fa6d15c4a6634e05668a8b483
Madeline Krieger <Madelineclaire12@gmail.com>
2022-09-26 11:53:23 -0700

    Update Phyloseq.R

--- Phyloseq.R
+++ Phyloseq.R
@@ -113,6 +113,92 @@ phylo_obj_f_pruned_fs_t_n_sglom <- tax_glom(phylo_obj_f_pruned_fs_t_n, taxrank =
 phylo_obj_f_pruned_fs_t_n_sglom_df <- psmelt(phylo_obj_f_pruned_fs_t_n_sglom)
 
 #Clean dataframe so that we don't have the s__ in front of Species and so there are no "_" characters
-#Species
 phylo_obj_f_pruned_fs_t_n_sglom_df$Species <- gsub("s__","",as.character(phylo_obj_f_pruned_fs_t_n_sglom_df$Species))
+
+                                                    
+################
+#ggplots Bar Plot
+################
+
+devtools::install_github("jaredhuling/jcolors")
+library(jcolors)
+display_jcolors("pal2")
+
+#Make sure all the abundances are 1
+aggregate(phylo_obj_f_pruned_fs_t_n_sglom_df$Abundance, list(phylo_obj_f_pruned_fs_t_n_sglom_df$Sample), FUN=sum)
+phylo_obj_f_pruned_fs_t_n_sglom_df
+
+
+phylo_obj_f_pruned_fs_t_n_sglom_df[phylo_obj_f_pruned_fs_t_n_sglom_df == "nucleatum subsp. animalis"] <- "animalis"
+phylo_obj_f_pruned_fs_t_n_sglom_df[phylo_obj_f_pruned_fs_t_n_sglom_df == "nucleatum subsp. nucleatum"] <- "nucleatum"
+phylo_obj_f_pruned_fs_t_n_sglom_df[phylo_obj_f_pruned_fs_t_n_sglom_df == "nucleatum subsp. fusiforme"] <- "vincentii"
+phylo_obj_f_pruned_fs_t_n_sglom_df[phylo_obj_f_pruned_fs_t_n_sglom_df == "nucleatum subsp. polymorphum"] <- "polymorphum"
+phylo_obj_f_pruned_fs_t_n_sglom_df[phylo_obj_f_pruned_fs_t_n_sglom_df == "nucleatum periodonticum"] <- "periodonticum"
+
+#For everything lumped together
+phylo_obj_f_pruned_fs_t_n_sglom_df%>%
+  ggplot(aes(x = Sample, y = Abundance, fill = Species, reorder_within(Species=="nucleatum subsp. animalis", Abundance)))+
+  scale_x_discrete(drop = TRUE)+
+  geom_bar(aes(), stat="identity", size=1)+
+  theme(strip.background = element_rect(color="black", fill="white", size=1.5, linetype="solid"))+
+  theme(axis.text.x=element_blank()) +
+  theme(legend.text=element_text(face="italic"))+ 
+  theme(strip.placement="top")+
+  facet_grid(~factor(Type, levels=c("Plaque", "Abscess")), scales="free") +
+  scale_fill_discrete(limits = c("animalis", "nucleatum", "perdiodonticum", "polymorphum", "vincentii"))+
+  scale_fill_manual(values=c("#5290f2", "#F9CBAD", "#969595", "#f5f573", "#4ae059"))
+# scale_fill_manual(values=c("#B4C8E8", "#F9CBAD", "#BFBFBF", "#FFE699", "#E3F1D9"))
+#scale_fill_jcolors("pal2")  
+
+ggsave(path = "~/Desktop/", filename="RelAbund.pdf", height =3.5, device='pdf', dpi=1000)
+                                                    
+                                                    
+################
+#CORRELATION MATRIX
+################
+library("ggpubr")
+library("Hmisc")
+library(tidyverse)
+library(corrplot)
+
+Counts_Fs_relabundance_paired_reformatted_forCor = subset(Counts_Fs_relabundance_paired_reformatted_forR, select = -c(1)) #remove the first row from the original df
+Counts_Fs_relabundance_paired_reformatted_forCor <- Counts_Fs_relabundance_paired_reformatted_forCor %>% remove_rownames %>% column_to_rownames(var="Patient") #turn the first row (patient) into row names
+
+Counts_Fs_relabundance_paired_reformatted_forCor2 <-
+  Counts_Fs_relabundance_paired_reformatted_forCor %>%
+  rename_all(funs(c("FFAbscess_animalis", "JJAbscess_fusiforme", "GGAbscess_nucleatum", "IIAbscess_polymorphum", "HHAbscess_periodonticum",
+                    "AAPlaque_animalis", "EEPlaque_fusiforme", "BBPlaque_nucleatum", "DDPlaque_polymorphum", "CCPlaque_periodonticum"))) %>%
+  select(AAPlaque_animalis, BBPlaque_nucleatum, CCPlaque_periodonticum, DDPlaque_polymorphum, EEPlaque_fusiforme, 
+         FFAbscess_animalis, GGAbscess_nucleatum, HHAbscess_periodonticum, IIAbscess_polymorphum, JJAbscess_fusiforme)
+
+Counts_Fs_relabundance_paired_reformatted_forCor2
+
+Res = rcorr(as.matrix(Counts_Fs_relabundance_paired_reformatted_forCor2), type="spearman")
+Res$P
+
+res2 = cor(Counts_Fs_relabundance_paired_reformatted_forCor2)
+testRes2 = cor.mtest(Counts_Fs_relabundance_paired_reformatted_forCor2,conf.level = 0.95)
+testRes2
+
+plot.new()
+corrplot(res2, p.mat = testRes2$p, sig.level = 0.05, 
+         type = "upper", insig='blank', addCoef.col ='black', 
+         order = "hclust", tl.cex = .6,
+         number.cex = 0.6, tl.col = "black", tl.srt = 45, col = COL2('BrBG'))
+
+
+png(width=7, height=5, units="in", res=800, file="~/Desktop/Cor-matrix.png")
+corrplot(res2, p.mat = testRes2$p, 
+         insig = 'label_sig', 
+         sig.level = c(0.001, 0.01, 0.05), 
+         type = "upper", 
+         tl.cex = .6, 
+         order = "alphabet",
+         pch.cex = 0.9, 
+         pch.col = 'grey20',
+         tl.col = "black", 
+         tl.srt = 45)
+dev.off()                                                    
+                                                    
+                                                  
 
